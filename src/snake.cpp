9a23bc9db76d1c06c10ad1a71b0a7252ee0908f5
KIM <dhdgn@naver.com>
2021-05-21 23:52:37 +0900

    choose stages

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,4 +1,6 @@
 #include "snake.h"
+#define COL 21
+#define ROW 21
 
 Snake::Snake() {
   initUI();
@@ -19,10 +21,35 @@ void Snake::initUI() {
   curs_set(0);
   // border
   border('*', '*', '*', '*', '*', '*', '*', '*');
+  // key input
+  keypad(stdscr, TRUE);
+  // no write in window
+  noecho();
+
+
   // text
   mvprintw(5, 5, "Snake Game!");
   for(int i=0; i<4; i++)
     mvprintw(i+6, 5, "%d. Stage %d" ,i+1, i+1);
+
+  // choose stage
+  while(true) {
+    char stage = getch();
+    if(stage=='1' || stage=='2' || stage=='3' || stage=='4')
+     break;
+  }
+  //Render render(stage);
+
+
+  // render game window
+  while(true) {
+    break;
+    for(int row=0; row<ROW; row++) {
+      for(int col=0; col<COL; col++) {
+        // mvprintw
+      }
+    }
+  }
 
   // refresh window
   refresh();
