36abf85d8f1a493d6a2237baed83f66cd92d62fc
MinSeo <hightuv@gmail.com>
2021-05-22 00:32:32 +0900

    Modified mistake

--- src/Map.h
+++ src/Map.h
@@ -4,7 +4,7 @@
 
 class Map {
   char mapData[21][21];
-  std::string mapFile = "../map/map"
+  std::string mapFile = "../map/map";
 public:
   void setStage(int stage=1);
   char getMapData(int row, int col);
