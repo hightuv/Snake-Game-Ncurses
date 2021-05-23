b95f98a16ac0e2e81f7b443f52936bd4d39c7c3b
MinSeo <hightuv@gmail.com>
2021-05-23 20:13:15 +0900

    Poison Item and Growth Item Printed on the Screen

--- src/render.cpp
+++ src/render.cpp
@@ -103,7 +103,7 @@ void Render::updateUI(int time) {
       else if (c == GROWTHITEM) {
         mvwprintw(snakeWindow, i, j*2, "ㅗ");
       }
-      else if (c == POISONITEM){
+      else if (c == POISONITEM) {
         mvwprintw(snakeWindow, i, j*2, "ㅇ");
       }
     }
@@ -128,8 +128,9 @@ void Render::updateMapData(int time) {
     spawnPoisonItem();
     spawnGrowthItem();
   }
-  mapDataArray[growthItem.first][growthItem.second] = GROWTHITEM;
   mapDataArray[poisonItem.first][poisonItem.second] = POISONITEM;
+  mapDataArray[growthItem.first][growthItem.second] = GROWTHITEM;
+
 }
 
 void Render::spawnGrowthItem() {
@@ -147,7 +148,7 @@ void Render::spawnGrowthItem() {
 
 void Render::spawnPoisonItem() {
   //time_t t = time(NULL);
-  srand(time(NULL));
+  srand(time(NULL) + 1);
   int poison_x = rand() % 19 + 1;
 	int poison_y = rand() % 19 + 1;
   while (mapDataArray[poison_x][poison_y] != EMPTY) {
