3997b9e833fb41184be2b8645b2701859cca14c0
MinSeo <hightuv@gmail.com>
2021-05-23 21:14:06 +0900

    Few updates on the event when snake meets items

--- src/snakeMove.h
+++ src/snakeMove.h
@@ -4,6 +4,7 @@
 class SnakeMove {
   int snakeHead[2];
   std::deque<std::pair<int,int>> snakeBody;
+  std::pair<int,int> tail;
 public:
   SnakeMove() {
     snakeHead[0] = 10, snakeHead[1] = 10;
@@ -12,21 +13,25 @@ public:
   }
   void changePos (int keyPressed) {
     if (keyPressed == KEY_UP) { // Up
+      tail = *(snakeBody.end() - 1);
       snakeBody.pop_back();
       snakeBody.push_front(std::pair<int,int>(snakeHead[0],snakeHead[1]));
       snakeHead[0] -= 1;
     }
     else if (keyPressed == KEY_DOWN) { // Down
+      tail = *(snakeBody.end() - 1);
       snakeBody.pop_back();
       snakeBody.push_front(std::pair<int,int>(snakeHead[0],snakeHead[1]));
       snakeHead[0] += 1;
     }
     else if (keyPressed == KEY_LEFT) { // Left
+      tail = *(snakeBody.end() - 1);
       snakeBody.pop_back();
       snakeBody.push_front(std::pair<int,int>(snakeHead[0],snakeHead[1]));
       snakeHead[1] -= 1;
     }
     else if (keyPressed == KEY_RIGHT) { // Right
+      tail = *(snakeBody.end() - 1);
       snakeBody.pop_back();
       snakeBody.push_front(std::pair<int,int>(snakeHead[0],snakeHead[1]));
       snakeHead[1] += 1;
@@ -39,7 +44,7 @@ public:
     return snakeBody[i];
   }
   void snakeHitGrowthItem(std::pair<int,int> growthItem) {
-    snakeBody.push_front(std::pair<int,int>(snakeHead[0],snakeHead[1]));
+    snakeBody.push_back(tail);
   }
   void snakeHitPoisonItem(std::pair<int,int> poisonItem) {
     snakeBody.pop_back();
